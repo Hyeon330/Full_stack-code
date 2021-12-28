0307591667975cf502e9aea6ce7ad23b8efa6bf9
이현수 <dldlsrn231@gmail.com>
2021-12-28 19:56:17 +0900

    update

--- HelloWorld/src/While.java
+++ HelloWorld/src/While.java
@@ -19,30 +19,30 @@ public class While {
          * 정답보다 크면 "더 작은수로 다시 시도해보세요."를 출력하고,
          * 정답보다 작으면 " 더 큰수로 다시 시도해보세요."를 출력
          */
-        // Scanner sc = new Scanner(System.in);
-        // final int com = (int) (Math.random() * 100) + 1;
-        // int user = 0;
-        // System.out.println(com);
-        // do {
-        // System.out.print("값 입력 : ");
-        // try {
-        // user = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        final int com = (int) (Math.random() * 100) + 1;
+        int user = 0;
+        System.out.println(com);
+        do {
+            System.out.print("값 입력 : ");
+            try {
+                user = sc.nextInt();
 
-        // if (com > user) {
-        // System.out.println("더 큰수로 다시 시도해보세요.");
-        // } else if (com < user) {
-        // System.out.println("더 작은수로 다시 시도해보세요.");
-        // } else {
-        // System.out.println("정답입니다!!");
-        // }
-        // } catch (Exception e) {
-        // System.out.println("올바른 값을 입력 해 주세요.");
-        // }
-        // sc.nextLine();
-        // System.out.println();
+                if (com > user) {
+                    System.out.println("더 큰수로 다시 시도해보세요.");
+                } else if (com < user) {
+                    System.out.println("더 작은수로 다시 시도해보세요.");
+                } else {
+                    System.out.println("정답입니다!!");
+                }
+            } catch (Exception e) {
+                System.out.println("올바른 값을 입력 해 주세요.");
+            }
+            sc.nextLine();
+            System.out.println();
 
-        // } while (com != user);
+        } while (com != user);
 
-        // sc.close();
+        sc.close();
     }
 }
