1ee13aca36fd55280410896ec37bedae268c859d
이현수 <dldlsrn231@gmail.com>
2022-01-03 03:17:11 +0900

    과제 Update

--- Assignment/src/Day_5_assign/soccer/Soccer.java
+++ Assignment/src/Day_5_assign/soccer/Soccer.java
@@ -2,13 +2,111 @@ package Day_5_assign.soccer;
 
 public class Soccer {
 
+    Soccer() {
+        System.out.println("축구 시뮬레이션 게임!!");
+        System.out.println("============================");
+        System.out.println("선수들의 이름을 정해주세요!!");
+        System.out.println();
+    }
+
+    int score1;
+    int score2;
+
+    public void Goal1() {
+        this.score1++;
+    }
+
+    public void Goal2() {
+        this.score2++;
+    }
+
 }
 
-class Team extends Soccer {
-    String name;
+class Team {
+    String teamName;
+    Player FW1 = new PlayerFW();
+    Player FW2 = new PlayerFW();
+    Player MD1 = new PlayerMD();
+    Player MD2 = new PlayerMD();
+    Player DF1 = new PlayerDF();
+    Player DF2 = new PlayerDF();
+    Player[] playerList = { FW1, FW2, MD1, MD2, DF1, DF2 };
+
+    public void playerNames() {
+        System.out.println(this.FW1.getName());
+        System.out.println(this.FW2.getName());
+        System.out.println(this.MD1.getName());
+        System.out.println(this.MD2.getName());
+        System.out.println(this.DF1.getName());
+        System.out.println(this.DF2.getName());
+    }
 
 }
 
-class Person extends Team {
-    String name;
+class Player {
+    private String name;
+    Boolean ball = false;
+    int baseShoot = 5; // 기본 슈팅 능력치
+    int basePass = 5; // 기본 패스 능력치
+    int baseTackle = 3; // 기본 태클 능력
+
+    public boolean ballStateChange() {
+        this.ball = !this.ball;
+        return this.ball;
+    }
+
+    public int shoot() {
+        int result = (int) (Math.random() * this.baseShoot);
+
+        return result;
+    }
+
+    public int pass() {
+        int result = (int) (Math.random() * this.basePass);
+
+        return result;
+    }
+
+    public int tackle() {
+        int result = (int) (Math.random() * this.baseTackle);
+
+        return result;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+class PlayerFW extends Player {
+    @Override
+    public int shoot() {
+        int result = (int) (Math.random() * super.baseShoot + 3); // 공격수 슈팅 능력
+
+        return result;
+    }
+}
+
+class PlayerMD extends Player {
+
+    @Override
+    public int pass() {
+        int result = (int) (Math.random() * super.basePass + 3); // 미드필더 패스 능력
+
+        return result;
+    }
+}
+
+class PlayerDF extends Player {
+
+    @Override
+    public int tackle() {
+        int result = (int) (Math.random() * super.baseTackle + 2); // 수비수 태클 능력
+
+        return result;
+    }
 }
--- Assignment/src/Day_5_assign/soccer/SoccerMain.java
+++ Assignment/src/Day_5_assign/soccer/SoccerMain.java
@@ -1,5 +1,156 @@
 package Day_5_assign.soccer;
 
 public class SoccerMain {
+    public static void main(String[] args) {
 
+        Soccer soccer = new Soccer();
+
+        String[] team1Names = { "메시", "날강두", "피를로", "베컴", "피케", "푸욜" };
+        String[] team2Names = { "손흥민", "케인", "박지성", "제라드", "라모스", "루이스" };
+        // 이름은 그냥 제가 넣어줬습니다.
+        // 원래는 직접 적어줘야 합니다.
+
+        Team team1 = new Team();
+        Team team2 = new Team();
+        Team[] allTeam = { team1, team2 };
+
+        for (int i = 0; i < team1Names.length; i++) {
+            team1.playerList[i].setName(team1Names[i]);
+        }
+        for (int i = 0; i < team2Names.length; i++) {
+            team2.playerList[i].setName(team2Names[i]);
+        }
+
+        System.out.println("경기 시작합니다!!");
+        System.out.println("==========================================================");
+
+        final int EVENT = 5;
+
+        team1.FW1.ballStateChange();
+        for (int a = 0; a <= 90; a += (int) (Math.random() * 20)) {
+            for (int j = 0; j < EVENT; j++) { // 1회마다 이벤트 발생 슛을 날릴시 1회 종료
+                System.out.println(j);
+                for (int t = 0; t < allTeam.length; t++) { // 공이 어떤 팀에 있는지 체크
+                    int shootCk = 0;
+                    int passCk = 0;
+                    for (int k = 0; k < allTeam[t].playerList.length; k++) { // 공이 누구에게 있는지 체크
+                        if (allTeam[t].playerList[k].ball) { // 공이 있는 사람을 찾으면 행동(이벤트: 슛, 패스, 태클 당함) 발생
+                            if (j == EVENT - 1) { // j가 마지막까지 오면 공을 갖고 있는 선수가 슛을 날림
+                                shootCk = 1; // 슛을 날렸는지 체크 값이 1이면 for(t)를 빠져나감
+                                System.out.println(allTeam[t].playerList[k].getName() + " 슛!!!");
+                                System.out.println();
+                                if (allTeam[t].playerList[k].shoot() > 2) { // 골이 들어감
+                                    if (t == 0) {
+                                        soccer.Goal1();
+                                    } else {
+                                        soccer.Goal2();
+                                    }
+                                    System.out.println("들어갔어요!!!");
+                                    System.out.println("현재 스코어 " + soccer.score1 + " : " + soccer.score2);
+                                } else { // 골이 빗나감
+                                    System.out.println("아!! 아쉽게 빗나갑니다.");
+                                }
+                                System.out.println();
+                                allTeam[t].playerList[k].ballStateChange(); // 현재 공을 들고 있는 선수가 공을 놈
+                                if (t == 0) { // 슛을 했기때문에 상대방 선수에게 공을 넘겨 줌
+                                    allTeam[1].playerList[0].ballStateChange();
+                                } else {
+                                    allTeam[0].playerList[0].ballStateChange();
+                                }
+                                break;
+                            } else { // 태클/패스 이벤트
+                                if ((int) (Math.random() * 4) == 0) { // 태클
+                                    int randPlayer = (int) (Math.random() * allTeam[t].playerList.length);
+                                    if (t == 0) {
+                                        System.out
+                                                .println(allTeam[1].playerList[randPlayer].getName() + "선수의 태클!!");
+
+                                        if (allTeam[1].playerList[randPlayer].tackle() > 1) {
+                                            System.out.println("실패합니다~~");
+                                        } else {
+                                            System.out.println("성공합니다!!");
+                                            allTeam[t].playerList[k].ballStateChange();
+                                            allTeam[1].playerList[randPlayer].ballStateChange();
+                                            System.out.println("공은 " + allTeam[t].playerList[randPlayer].getName()
+                                                    + "선수에서 " + allTeam[1].playerList[randPlayer].getName()
+                                                    + "선수로 넘어갑니다!!");
+                                        }
+
+                                    } else {
+                                        System.out
+                                                .println(allTeam[0].playerList[randPlayer].getName() + "선수의 태클!!");
+
+                                        if (allTeam[0].playerList[randPlayer].tackle() > 1) {
+                                            System.out.println("실패합니다~~");
+                                        } else {
+                                            System.out.println("성공합니다!!");
+                                            allTeam[t].playerList[k].ballStateChange();
+                                            allTeam[0].playerList[randPlayer].ballStateChange();
+                                            System.out.println("공은 " + allTeam[t].playerList[randPlayer].getName()
+                                                    + "선수에서 " + allTeam[0].playerList[randPlayer].getName()
+                                                    + "선수로 넘어갑니다!!");
+                                        }
+                                    }
+                                    System.out.println();
+                                    j = EVENT - j;
+
+                                } else { // 패스
+                                    int nextPlayer = 0;
+                                    while (true) {
+                                        nextPlayer = (int) (Math.random() * allTeam[t].playerList.length);
+                                        if (!allTeam[t].playerList[nextPlayer].ball) { // 패스할 시 같은 선수 나오는 것 방지
+                                            break;
+                                        }
+                                    }
+                                    allTeam[t].playerList[k].ballStateChange();
+                                    allTeam[t].playerList[nextPlayer].ballStateChange();
+                                    System.out.println(allTeam[t].playerList[k].getName() + " 선수가 "
+                                            + allTeam[t].playerList[nextPlayer].getName() + "에게 패스합니다.");
+                                    System.out.println();
+                                    if (allTeam[t].playerList[k].pass() == 0) { // 패스 실수
+                                        nextPlayer = (int) (Math.random() * allTeam[t].playerList.length);
+                                        int whatTeam = (int) (Math.random() * 2);
+
+                                        allTeam[t].playerList[k].ballStateChange();
+
+                                        System.out.println(
+                                                "아!!" + allTeam[t].playerList[k].getName() + "선수 왜 저렇게 패스하죠?!");
+                                        allTeam[whatTeam].playerList[nextPlayer].ballStateChange();
+                                        // 랜덤한 선수에게 공이감
+                                        System.out
+                                                .println("공은 " + allTeam[whatTeam].playerList[nextPlayer].getName()
+                                                        + "선수에게 가게 됩니다.");
+                                        if (t != whatTeam) {
+                                            j = EVENT - j;
+                                        }
+                                        System.out.println();
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                        if (shootCk == 1 || passCk == 1) { // 슛을 날렸는지 체크 값이 1이면 for(k)를 빠져나감
+                            break;
+                        }
+                    }
+                    if (shootCk == 1 || passCk == 1) { // 슛을 날렸는지 체크 값이 1이면 for(t)를 빠져나감
+                        break;
+                    }
+                }
+            }
+
+        }
+        System.out.println("==========================================================");
+        System.err.println("경기 종료합니다!!!");
+
+        System.out.println("최종 스코어 " + soccer.score1 + " : " + soccer.score2);
+
+        if (soccer.score1 > soccer.score2) {
+            System.out.println("Team1 win!!");
+        } else if (soccer.score1 < soccer.score2) {
+            System.out.println("Team2 win!!");
+        } else {
+            System.out.println("무승부");
+        }
+    }
 }
