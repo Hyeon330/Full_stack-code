13cd744c487b3d8a775aad7e23058690de8c9a9d
이현수 <dldlsrn231@gmail.com>
2022-01-09 00:32:20 +0900

    update

--- Assignment/src/week_two/airplane/AirMain.java
+++ Assignment/src/week_two/airplane/AirMain.java
@@ -6,12 +6,14 @@ public class AirMain {
     public static void main(String[] args) {
         Calendar calD = Calendar.getInstance();
         Calendar calA = Calendar.getInstance();
-        AirplaneSet airSet = new AirplaneSet();
+        AirplaneChild airSet = new AirplaneChild();
 
         calD.set(2022, 1, 1);
         calA.set(2022, 1, 2);
 
         airSet.setDepartCal(calD);
         airSet.setDepartCal(calA);
+
+        airSet.OutputDepartDate();
     }
 }
--- Assignment/src/week_two/airplane/Airplane.java
+++ Assignment/src/week_two/airplane/Airplane.java
@@ -1,11 +1,15 @@
 package week_two.airplane;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 
 public abstract class Airplane {
 
     private Calendar DepartCal;
     private Calendar ArrivalCal;
+    private String isDomestic;
 
     public Calendar getDepartCal() {
         return this.DepartCal;
@@ -23,9 +27,27 @@ public abstract class Airplane {
         this.ArrivalCal = ArrivalCal;
     }
 
+    public void OutputDepartDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        System.out.println(sdf.format(getDepartCal().getTime()));
+    }
+
+    public void OutputArrivalDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        System.out.println(sdf.format(getArrivalCal().getTime()));
+    }
+
+    abstract void Domestic(String Domestic);
+
+    abstract void Overseas(String Overseas);
+
+    abstract String flight(String Domestic);
+
 }
 
-class AirplaneSet extends Airplane {
+class AirplaneChild extends Airplane {
+    private ArrayList<String> domesticArr = new ArrayList<String>(Arrays.asList("김포", "인천", "김해", "제주", "울산"));
+    private ArrayList<String> overseasArr = new ArrayList<String>(Arrays.asList("도쿄", "상하이", "홍콩", "싱가폴", "쿠알라룸푸르"));
 
     @Override
     public void setDepartCal(Calendar DepartCal) {
@@ -37,6 +59,30 @@ class AirplaneSet extends Airplane {
     public void setArrivalCal(Calendar ArrivalCal) {
         ArrivalCal = Calendar.getInstance();
         super.setArrivalCal(ArrivalCal);
+    }
+
+    @Override
+    void Domestic(String Domestic) {
+        this.domesticArr.add(Domestic);
+    }
+
+    @Override
+    void Overseas(String Overseas) {
+        this.overseasArr.add(Overseas);
+    }
+
+    ArrayList<String> Domestic() {
+        return domesticArr;
+    }
+
+    ArrayList<String> Overseas() {
+        return overseasArr;
+    }
+
+    @Override
+    String flight(String Domestic) {
+        Domestic();
+        return null;
     }
 
 }
