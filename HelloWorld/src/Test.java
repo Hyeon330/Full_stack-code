d6d35e1dcaed986353dcb7a504ef00857352d0c7
이현수 <dldlsrn231@gmail.com>
2022-01-13 17:58:22 +0900

    Update

--- HelloWorld/src/Test.java
+++ HelloWorld/src/Test.java
@@ -1,46 +1,19 @@
-enum Direction {
-    EAST, SOUTH, WEST, NORTH
-}
+import java.util.HashMap;
+
+import javax.activity.InvalidActivityException;
 
 public class Test {
     public static void main(String[] args) {
-        Direction d1 = Direction.EAST;
-        Direction d2 = Direction.valueOf("WEST");
-        Direction d3 = Enum.valueOf(Direction.class, "EAST");
+        HashMap map = new HashMap();
+        map.put("홍길동", new Integer(90));
+        map.put("홍길동", new Integer(100));
 
-        System.out.println("d1=" + d1);
-        System.out.println("d2=" + d2);
-        System.out.println("d3=" + d3);
+        Set set = map.entrySet();
+        Iterator it = set.iterator();
 
-        System.out.println("d1==d2 ? " + (d1 == d2));
-        System.out.println("d1==d3 ? " + (d1 == d3));
-        System.out.println("d1.equals(d3) ? " + d1.equals(d3));
-        // System.out.println("d2 > d3 ? " + (d1 > d3)); // 에러
-        System.out.println("d1.compareTo(d3) ? " + (d1.compareTo(d3)));
-        System.out.println("d1.compareTo(d2) ? " + (d1.compareTo(d2)));
-
-        switch (d1) {
-            case EAST:
-                System.out.println("The direction is EAST.");
-                break;
-            case WEST:
-                System.out.println("The direction is EAST.");
-                break;
-            case SOUTH:
-                System.out.println("The direction is EAST.");
-                break;
-            case NORTH:
-                System.out.println("The direction is EAST.");
-                break;
-            default:
-                System.out.println("Invalid direction.");
-                break;
-        }
-
-        Direction[] dArr = Direction.values();
-
-        for (Direction d : dArr) {
-            System.out.printf("%s=%d%n", d.name(), d.ordinal());
+        while (it.hasNext()) {
+            Map.Entry e = (Map.Entry) it.next();
+            System.out.println("이름 : " + e.getKey() + ", 점수 : " + e.getValue());
         }
     }
 }
