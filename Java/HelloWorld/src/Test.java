b4e5bf6d747c7ec9a566454ed5441c690187a3c6
이현수 <dldlsrn231@gmail.com>
2022-01-21 00:41:53 +0900

    update

--- Java/HelloWorld/src/Test.java
+++ Java/HelloWorld/src/Test.java
@@ -1,49 +1,19 @@
 import java.util.*;
 
 public class Test {
-    public static void main(String[] args) throws Exception {
-        ArrayList list1 = new ArrayList(10);
-        list1.add(new Integer(5));
-        list1.add(new Integer(4));
-        list1.add(new Integer(2));
-        list1.add(new Integer(0));
-        list1.add(new Integer(1));
-        list1.add(new Integer(3));
+    public static void main(String[] args) {
+        Queue pq = new PriorityQueue();
+        pq.offer(3);
+        pq.offer(1);
+        pq.offer(5);
+        pq.offer(2);
+        pq.offer(4);
+        System.out.println(pq); // pq의 내부 배열을 출력
 
-        ArrayList list2 = new ArrayList(list1.subList(1, 4));
-        print(list1, list2);
+        Object obj = null;
 
-        Collections.sort(list1); // list1과 list2를 정렬한다.
-        Collections.sort(list2); // Collections.sort(List 1)
-        print(list1, list2);
-
-        System.out.println("list1.containsAll(list2):" + list1.containsAll(list2));
-
-        list2.add("B");
-        list2.add("C");
-
-        list2.add(2, "A");
-        print(list1, list2);
-
-        list2.add(2, "A");
-        print(list1, list2);
-
-        // list1에서 list2와 겹치는 부분만 남기고 나머지는 삭제한다.
-        System.out.println("list1.retainAll(list2):" + list1.retainAll(list2));
-
-        print(list1, list2);
-
-        // list2에서 list1에 포함된 객체들을 삭제한다.
-        for (int i = list2.size() - 1; i >= 0; i--) {
-            if (list1.contains(list2.get(i)))
-                list2.remove(i);
-        }
-        print(list1, list2);
-    }
-
-    static void print(ArrayList list1, ArrayList list2) {
-        System.out.println("list1:" + list1);
-        System.out.println("list2:" + list2);
-        System.out.println();
+        // PriorityQueue에 저장된 요소를 하나씩 꺼낸다.
+        while ((obj = pq.poll()) != null)
+            System.out.println(obj);
     }
 }
